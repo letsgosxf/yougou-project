f0e43e1e4e5480d6207ea1bb1cba6d875c44f88f
letsgoFxs <wysxf1030@163.com>
2022-04-18 01:07:30 +0800

    入口文件样式处理

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function () {},
+  onShow: function () {},
+  onHide: function () {},
+};
 </script>
 
-<style>
-	/*每个页面公共css */
+<style lang="scss">
+/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
+@import "uview-ui/index.scss";
+.status_bar {
+  height: var(--status-bar-height);
+  width: 100%;
+}
 </style>
