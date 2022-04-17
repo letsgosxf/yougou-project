f1a4b59963aacc03f13b17c377585cfbd713eff1
letsgoFxs <wysxf1030@163.com>
2022-04-18 01:09:55 +0800

    头部组件封装

---
+++ src/components/ygHeader/ygHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <u-navbar
+    :is-back="isBack"
+    back-icon-color="#fff"
+    :title="headerTitle"
+    :title-size="titleSize"
+    :title-color="titleColor"
+    :background="background"
+    :border-bottom="false"
+  ></u-navbar>
+</template>
+
+<script>
+export default {
+  props: {
+    headerTitle: {
+      type: String,
+      require: true,
+    },
+    isBack: {
+      type: Boolean,
+    },
+  },
+  data() {
+    return {
+      background: {
+        backgroundColor: "#eb4450",
+      },
+      titleSize: 32,
+      titleColor: "#fceeef",
+    };
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss"></style>
