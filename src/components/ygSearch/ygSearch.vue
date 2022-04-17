0b071011db456865cffb824b896d8d562a9c34e3
letsgoFxs <wysxf1030@163.com>
2022-04-18 01:10:05 +0800

    搜索框组件封装

---
+++ src/components/ygSearch/ygSearch.vue
@@ -0,0 +1,31 @@
+<template>
+  <view class="my-search">
+    <u-button :custom-style="customStyle" :hair-line="isHairLine"
+      >搜&nbsp;索</u-button
+    >
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      customStyle: {
+        width: "700rpx",
+        height: "80rpx",
+      },
+      isHairLine: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.my-search {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100rpx;
+  background-color: $theme-color;
+}
+</style>
