31d18f44e63cde9fb658223f0df65e1d427e155f
letsgoFxs <wysxf1030@163.com>
2022-04-18 16:42:32 +0800

    商品详情布局完成

--- src/pages/goods_detail/goods_detail.vue
+++ src/pages/goods_detail/goods_detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <view>
+  <view v-if="detail.goods_name">
     <view class="header-content">
       <ygHeader :headerTitle="title" :isBack="isBack"></ygHeader>
     </view>
@@ -18,7 +18,7 @@
       <view class="goods-top">
         <view class="goods-price">
           <span class="goods-icon">￥</span>
-          <span class="goods-number">39999</span>
+          <span class="goods-number">{{ detail.goods_price }}</span>
         </view>
         <view class="goods-share">
           <u-icon name="zhuanfa" color="#707070" size="36"></u-icon>
@@ -26,24 +26,43 @@
         </view>
       </view>
       <view class="goods-bottom">
-        国产贵妃柑1kg 新鲜水果 柑橘 橘子 国产贵妃柑1kg 新鲜水果 柑橘
-        橘子国产贵妃柑1kg 新鲜水果 柑…
+        {{ detail.goods_name }}
       </view>
     </view>
+    <!-- 分隔线 -->
+    <view class="divider"></view>
+    <!-- 富文本内容 -->
+    <u-parse
+      v-if="lazyloadImage.length > 0"
+      :html="detail.goods_introduce"
+      :selectable="true"
+      :lazy-load="true"
+      :loading-img="lazyloadImage"
+      :show-with-animation="true"
+    ></u-parse>
+    <!-- 底部组件 -->
+    <SubmitBar @addCart="addCart" />
   </view>
 </template>
 
 <script>
+import SubmitBar from "./components/submit_bar";
 let params = {
   goods_id: "",
 };
 export default {
+  components: {
+    SubmitBar,
+  },
   data() {
     return {
       title: "商品详情",
       isBack: true,
+      lazyloadImage:
+        "https://www.google.com.hk/url?sa=i&url=http%3A%2F%2F616pic.com%2Ftupian%2Fjiazaizhong.html&psig=AOvVaw0TLpSGJ-FNxn5dL-WfdEYZ&ust=1650349878944000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJiO5cb-nPcCFQAAAAAdAAAAABAD",
       detail: {},
       swiperList: [],
+      cart: [],
     };
   },
   computed: {
@@ -56,7 +75,7 @@ export default {
     },
   },
   onLoad(option) {
-    params.goods_id = option.goods_id || 25965;
+    params.goods_id = option.goods_id || 9832;
     // 获取数据
     this.getgoodsDetail();
   },
@@ -73,6 +92,27 @@ export default {
         urls: this.swiperList.map((item) => item.pics_big_url),
         indicator: "number",
       });
+    },
+    addCart() {
+      const index = this.cart.findIndex(
+        (item) => item.goods_id === Number(params.goods_id)
+      );
+      if (index === -1) {
+        this.cart.push({
+          goods_id: this.detail.goods_id,
+          goods_name: this.detail.goods_name,
+          goods_price: this.detail.goods_price,
+          number: 1,
+          pics: this.detail.pics[0].pics_sma_url,
+        });
+      } else {
+        this.cart = this.cart.map((ele, i) => {
+          if (i === index) {
+            ele.number++;
+          }
+          return ele;
+        });
+      }
     },
   },
 };
@@ -142,7 +182,14 @@ export default {
   .goods-bottom {
     color: #434343;
     font-size: 30rpx;
+    font-weight: bold;
     line-height: 42rpx;
   }
+}
+
+.divider {
+  width: 100%;
+  height: 20rpx;
+  background-color: #f9f9f9;
 }
 </style>
